0fdb39a70d1e50b1ef922e556d982fcdf976cd1d
hezhao <1439293823@qq.com>
2018-04-17 00:59:57 +0800

    提交

---
+++ SayWord/Form1.Designer.cs
@@ -0,0 +1,93 @@
+﻿namespace SayWord
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(350, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(134, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "开始朗读";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.Location = new System.Drawing.Point(12, 36);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox1.Size = new System.Drawing.Size(472, 174);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(161, 12);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "在这里输入你想朗读的文字：";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(496, 285);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ SayWord/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;//引入命名空间
+
+namespace SayWord
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        SpeechSynthesizer synth = new SpeechSynthesizer();
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //从.NET Framework 3.0开始，Framework提供了Speech API调用方法，这样就非常流畅完美了，不用再纠结非托管的COM了。
+            //首先、右键项目、添加引用
+            //左边分类选择框架、右边找到System.Speech
+            //然后在你的程序中添加using引用
+            //using System.Speech.Synthesis;
+            //最后调用SpeechSynthesizer对象的Speak方法朗读即可
+           
+           // synth.Speak("韩伟是谁啊，他喜欢谁啊？哈哈哈！！");
+
+            //这里会出现一个比较烦人的问题，Speak方法时单线程的，也就是说，在它朗读的过程中，当前程序的其他操作会全部卡掉、
+            //等其朗读完毕，就又可以继续操作了、
+
+            //Async：异步的
+            synth.Rate = 3;//语速-10~10
+            if (this.textBox1.Text.Trim().Length ==0)
+            {
+                 synth.SpeakAsync("请输入内容");
+            }
+            else
+            {
+                synth.SpeakAsync(this.textBox1.Text);//开始朗读，传入指定的内容
+            }
+           // 然后就惊喜的发现、朗读的时候不卡了、呵呵、其他的还有设置声音(synth.Volumn)、语速(synth.Rate)等等、
+            //大家可以百度SpeechSynthesizer的使用、很多例子。
+        }
+    }
+}
